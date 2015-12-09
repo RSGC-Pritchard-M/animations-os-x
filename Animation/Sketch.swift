ae914c791c0ce121b19c5a602246e0777b0a3c6c
RSGC-Pritchard-M <Matthew@MacBook-Pro704-Pritchard.local>
2015-12-09 00:05:21 -0500

    Attempt to get the ball moving all directions
    Attempt to get the ball moving all directions but can’t quite get it to
    work

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -16,13 +16,18 @@ class Sketch {
     
     // Declare any properties you need for your sketch below this comment, but before init()
     var x = 0
-    var s = 1
+    var sx = 1
+    var sy = 1
+    var y = 250
+    //var a = 1
+    //var b = 100
+    //var c = 0
 
     // This runs once, equivalent to setup() in Processing
     init() {
         
         // Create canvas object – specify size
-        canvas = Canvas(width: 700, height: 100)
+        canvas = Canvas(width: 400, height: 500)
         
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
@@ -33,12 +38,16 @@ class Sketch {
     func draw() {
                 
         // Horizontal position of circle
-        x = x + s
-        
+        x = x + sx
+        y = y + sy
         // Bounce when hitting wall
         if (x > canvas.width || x < 0) {
-            s *= -1
+            sx *= -1
         }
+        if (y > 500 || y < 0){
+            sy *= -1
+        }
+        //a + (canvas.framesPerSecond - b)*(x)+ c
         
         // Clear the background
         canvas.drawShapesWithBorders = false
